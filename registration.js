e7e2f4f3963bba5c85d9a6e9e79c485cae7b68ae
suraj7852 <surajpj7852@gmail.com>
2019-12-28 15:55:23 +0530

    Added validator for email

--- registration.js
+++ registration.js
@@ -1,6 +1,7 @@
 let input = require('readline-sync');
 let firstName = input.question("Enter your name: ");
 let nameRegex = RegExp('^[a-zA-Z ]{3,30}$');
+let emailRegex = RegExp('^[0-9a-zA-Z]+([-,_,+,.]{1}[0-9A-Za-z]+){0,1}@[0-9A-Za-z]+.[A-Za-z]{1,3}(.[a-zA-Z]{1,3}){0,1}$');
 if(nameRegex.test(firstName))
 	console.log("valid firstName");
 else
@@ -11,4 +12,11 @@ if(nameRegex.test(lastName))
         console.log("valid lastName");
 else
         console.log("Not valid");
+
+let email = input.question("Enter email id");
+if(emailRegex.test(email))
+        console.log("valid Email");
+else
+        console.log("Not valid");
+
 
