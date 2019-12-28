a9c06d1dad6ebe7b15f71929e61865551ac95906
suraj7852 <surajpj7852@gmail.com>
2019-12-28 15:41:02 +0530

    Added validator for last name

--- registration.js
+++ registration.js
@@ -1,9 +1,14 @@
 let input = require('readline-sync');
-let firstName = input.question("Enter your name");
-let firstNameRegex = RegExp('^[a-zA-Z ]{3,30}$');
-if(firstNameRegex.test(firstName))
+let firstName = input.question("Enter your name: ");
+let nameRegex = RegExp('^[a-zA-Z ]{3,30}$');
+if(nameRegex.test(firstName))
 	console.log("valid firstName");
 else
 	console.log("Not valid");
 
+let lastName = input.question("Enter last name: ");
+if(nameRegex.test(lastName))
+        console.log("valid lastName");
+else
+        console.log("Not valid");
 
